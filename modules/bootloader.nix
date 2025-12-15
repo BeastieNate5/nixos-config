3232c4fc11d1fbe9717c4d83e0404ebaf67714a7
0x2B <nhagan345@gmail.com>
2025-12-15 12:30:30 -0500

    Switched to GRUB

--- modules/bootloader.nix
+++ modules/bootloader.nix
@@ -1,6 +1,22 @@
+{ ... }:
 {
-    boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.grub.enable = true;
     boot.loader.grub.device = "nodev";
     boot.loader.grub.efiSupport = true;
+    /*
+    boot.loader.grub.theme = pkgs.stdenv.mkDerivation {
+        pname = "Futaba Grub Theme";
+        version = "0.0.1";
+        src = fetchTarball {
+            url = "https://github.com/SiriusAhu/Persona_5_Royal_Grub_Themes/releases/download/v1.0/navi.tar.gz";
+            sha256 = "sha256:0zd3yh04xg44vp4v7a03kv1p0nwlk2clkn1r0jimnd8sxxvqbalg";
+        };
+
+        installPhase = ''
+            mkdir -p $out/navi
+            cp -r ./* $out/navi/
+        '';
+    } + "/navi/theme.txt";
+    */
 }
