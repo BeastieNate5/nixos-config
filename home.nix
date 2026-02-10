85ba638f5e0a020e242b7edf8551c727b9356b47
0x2B <nhagan345@gmail.com>
2026-02-10 14:59:48 -0500

    Added zsh and starship to hm

--- home.nix
+++ home.nix
@@ -6,5 +6,24 @@
     home.stateVersion = "25.05";
 
     programs.home-manager.enable = true;
+
+    programs.zsh = {
+        enable = true;
+        oh-my-zsh = {
+            enable = true;
+            plugins = ["git" "colored-man-pages" "zsh-syntax-highlighting" "zsh-autosuggestions" "nix-shell"];
+        };
+    };
+
+    programs.starship.enable = true;
+
+    home.sessionVariables = {
+        EDITOR = "nvim";
+        VISUAL = "nvim";
+        HISTFILE = "$HOME/.zhistory";
+        HISTSIZE = 1000;
+        SAVEHIST = 1000;
+        _JAVA_AWT_WM_NONREPARENTING = 1;
+    };
 }
 
