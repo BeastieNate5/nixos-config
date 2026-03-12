327ca576edfd8a7504c054491eec6d873479ab1b
0x2B <nhagan345@gmail.com>
2026-03-12 14:33:05 -0400

    nvim uses system clipboard

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -7,5 +7,6 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
+vim.opt.clipboard = "unnamedplus"
 --vim.cmd.colorscheme("catppuccin")
 --vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
