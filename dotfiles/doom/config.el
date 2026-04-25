2a1d99d70085c58a5fd5a1d48fdd42c1cf5fa060
0x2B <nhagan345@gmail.com>
2026-04-24 22:12:01 -0400

    Emacs file change

--- dotfiles/doom/config.el
+++ dotfiles/doom/config.el
@@ -73,3 +73,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(set-frame-parameter nil 'alpha-background 80) ; For current frame
+(add-to-list 'default-frame-alist '(alpha-background . 80)) ; For all future frames
