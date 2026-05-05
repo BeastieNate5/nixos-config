d09026c943d2dac6e89f602f106cabc370380ccd
0x2B <nhagan345@gmail.com>
2026-05-05 18:23:54 -0400

    Removed all home-manager files

--- home-manager/modules/easyeffects/default.nix
+++
@@ -1,8 +0,0 @@
-{
-  services.easyeffects = {
-    enable = true;
-    extraPresets = {
-      base-enhacning-perfect-eq = builtins.fromJSON (builtins.readFile ./presets/Bass_Enhancing_Perfect_EQ.json);
-    };
-  };
-}
